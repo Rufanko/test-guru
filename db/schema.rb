38e4736f9322556eb7e7d612a5f5d37e40d57d44
Rufanko <rufanko@mail.ru>
2019-11-07 06:50:10 +0300

    change column default

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_07_031649) do
+ActiveRecord::Schema.define(version: 2019_11_07_034338) do
 
   create_table "answers", force: :cascade do |t|
-    t.boolean "correct", null: false
+    t.boolean "correct", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2019_11_07_031649) do
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
-    t.integer "level"
+    t.integer "level", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
