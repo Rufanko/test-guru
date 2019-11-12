0cc3aaa7a11a0a140bf97afc96c226a4f4a57d47
Rufanko <rufanko@mail.ru>
2019-11-12 06:09:07 +0300

    comment deleted

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
-
 class Test < ApplicationRecord
   def self.all_tests_by_category(category)
     Test.joins('INNER JOIN categories ON  categories.id = tests.category_id' ).where(categories: { name: category })
                                                                      .order(id: :desc)
-                                                                     #.pluck(:title)
+                                                                     .pluck(:title)
   end
 end
