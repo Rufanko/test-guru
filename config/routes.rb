6731988d6787694cc40a3f7dd6b57bb2b7080176
Rufanko <rufanko@mail.ru>
2019-12-07 00:00:03 +0300

    controller, view added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
   #get 'test_passages/show'
   #get 'test_passages/update'
   root to: 'tests#index'
+  get :signup, to: 'users#new'
 
+  resources :users, only: :create
 
   resources :tests do
   	resources :questions, except: :index, shallow: true do
--- db/migrate/20191113001952_create_users_tests_join_table.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsersTestsJoinTable < ActiveRecord::Migration[6.0]
-  def change
-    create_join_table :users, :tests do |t|
-       t.index [:user_id, :test_id]
-    end
-    change_column_null :tests_users, :user_id, false
-    change_column_null :tests_users, :test_id, false
-  end
-end
