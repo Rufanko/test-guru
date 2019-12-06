34d061306e83e5392e61165517a852b39e58d451
Rufanko <rufanko@mail.ru>
2019-12-07 00:12:50 +0300

    module Auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,10 @@
 class User < ApplicationRecord
-	attr_reader :password
-	attr_writer :password_confirmation
+
+	include Auth
 
 	has_many :test_passages, dependent: :destroy
 	has_many :tests, through: :test_passages
 	has_many :authors_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :nullify
-
-  validates :email, presence: true
-	validates :password, presence: true, if: Proc.new {|u| u.password_digest.blank? }
-	validates :password, confirmation: true
 
 	def find_by_level(level)
 	  tests.where(level: level)
@@ -16,25 +12,6 @@ class User < ApplicationRecord
 
 	def test_passage(test)
 		self.test_passages.order(id: :desc).find_by(test: test)
-	end
-
-	def authenticate(password_string)
-		digest(password_string) == self.password_digest ? self : false
-	end
-
-	def password=(password_string)
-		if password_string.nil?
-			self.password_digest = nil
-		elsif password_string.present?
-		  @password = password_string
-		  self.password_digest = digest(password_string)
-	  end
-	end
-
-	private
-
-	def digest(string)
-		Digest::SHA1.hexdigest(string)
 	end
 
 end
