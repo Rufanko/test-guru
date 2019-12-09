efced690fd35b257209e6483d96605cefd91c1ea
Rufanko <rufanko@mail.ru>
2019-12-09 06:51:51 +0300

    final changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to session[:custom_path]
+      session.delete[:custom_path]
     else
       flash.now[:alert] = 'Try again'
       render :new
