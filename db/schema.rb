f8a1a1ff8206401fbcc5969532e8d7a2d93cd6c9
Rufanko <rufanko@mail.ru>
2019-12-13 03:33:13 +0300

    Add name and type attributes to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_10_225432) do
+ActiveRecord::Schema.define(version: 2019_12_13_002834) do
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: false
@@ -77,8 +77,12 @@ ActiveRecord::Schema.define(version: 2019_12_10_225432) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "type", default: "User", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["type"], name: "index_users_on_type"
   end
 
   add_foreign_key "answers", "questions"
