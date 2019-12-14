06d528fce07eed6482a77cd64ae59d78e90fb2be
Rufanko <rufanko@mail.ru>
2019-12-14 05:50:26 +0300

    tests controller fixes

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -13,7 +13,7 @@ class Admin::QuestionsController < ApplicationController
   def create
     @question = @test.questions.new(question_params)
     if @question.save
-      redirect_to @test
+      redirect_to admin_test_path(@test)
     else
       render :new
      end
@@ -23,12 +23,12 @@ class Admin::QuestionsController < ApplicationController
 
   def destroy
     @question.destroy
-    redirect_to @question.test
+    redirect_to admin_test_path(@question.test)
   end
 
   def update
     if @question.update(question_params)
-      redirect_to @question.test
+      redirect_to admin_test_path(@question.test)
     else
       render :edit
     end
