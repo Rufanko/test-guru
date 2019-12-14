1126bd3d053fef9221e422b93ea139754df1c190
Rufanko <rufanko@mail.ru>
2019-12-14 05:45:47 +0300

    Add answers controller and views

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,44 +1,12 @@
 class AnswersController < ApplicationController
-  before_action :set_answer, only: %i[show edit update destroy  ]
-  before_action :find_question, only: %i[ new  create]
+  before_action :set_answer, only: %i[show]
   before_action :authenticate_user!
   def index
     @answers = Answer.all
   end
 
-  def show
-  end
+  def show; end
 
-
-  def new
-    @answer = @question.answers.new
-  end
-
-
-  def edit
-  end
-
-  def create
-    @answer = @question.answers.new(answer_params)
-
-      if @answer.save
-        redirect_to @answer, notice: 'answer was succesfuly create'
-      else
-        render :new
-      end
-  end
-
-   def update
-
-      if @answer.update(answer_params)
-        redirect_to @answer.question
-      end
-    end
-
-   def destroy
-    @answer.destroy
-    redirect_to @answer.question, notice: 'succes'
-   end
 
   private
     def set_answer
@@ -47,9 +15,5 @@ class AnswersController < ApplicationController
 
     def answer_params
       params.require(:answer).permit(:content, :correct)
-    end
-
-    def find_question
-      @question = Question.find(params[:question_id])
     end
 end
