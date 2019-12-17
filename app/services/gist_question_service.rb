0feff60c47adca8c421e3a10df488f77076a90b7
Rufanko <rufanko@mail.ru>
2019-12-17 05:56:16 +0300

    rubocop autocorrect

--- app/services/gist_question_service.rb
+++ app/services/gist_question_service.rb
@@ -1,32 +1,30 @@
 class GistQuestionService
+  def initialize(question, client: nil)
+    @question = question
+    @test = @question.test
+    @client = client || GitHubClient.new
+  end
 
-def initialize(question, client: nil)
-  @question = question
-  @test = @question.test
-  @client = client || GitHubClient.new
-end
+  def call
+    @client.create_gists(gist_params)
+  end
 
-def call
-  @client.create_gists(gist_params)
-end
+  private
 
-private
-
-def gist_params
-  {
-  "description": "A question about #{@test.title} from TestGuru",
-  "files": {
-    "test-guru-question.txt": {
-      "content": gist_content
+  def gist_params
+    {
+      "description": "A question about #{@test.title} from TestGuru",
+      "files": {
+        "test-guru-question.txt": {
+          "content": gist_content
+        }
+      }
     }
-  }
-}
-end
+  end
 
-def gist_content
-  content  = [@question.content]
-  content += @question.answers.pluck(:content)
-  content.join("\n")
-end
-
+  def gist_content
+    content  = [@question.content]
+    content += @question.answers.pluck(:content)
+    content.join("\n")
+  end
 end
