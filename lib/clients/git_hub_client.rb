2a97343ccdf4fae87a80e4e20660ebf233bd04d0
Rufanko <rufanko@mail.ru>
2019-12-17 05:31:10 +0300

    Add create gists method

--- lib/clients/git_hub_client.rb
+++ lib/clients/git_hub_client.rb
@@ -1,9 +1,18 @@
 class GitHubClient
 
   ROOT_ENDPOINT = 'https://api.github.com'
+  ACCES_TOKEN = '15ccf4b95142d64b636cda27e701a8953afc48ba'
 
   def initialize
     @http_client = setup_http_client
+  end
+
+  def create_gists(params)
+    @http_client.post('gists', params) do |request|
+      request.headers['Authorization'] = "token #{ACCES_TOKEN}"
+      request.headers['Content-Type'] = 'application/json'
+      request.body = params.to_json
+    end
   end
 
   private
