cae8475f08c3b43d6f5359e26ee3a30be215af83
Rufanko <rufanko@mail.ru>
2019-12-30 05:18:10 +0300

    Add passwords check

---
+++ app/javascript/utilities/passwords_check.js
@@ -0,0 +1,24 @@
+document.addEventListener('turbolinks:load', function() {
+var control = document.getElementById('user_password_confirmation')
+
+if (control) {control.addEventListener("input", checkpasswords)}
+})
+
+function checkpasswords() {
+  var password = document.getElementById('user_password').value
+  var password_confirmation = this.value
+
+  if (password_confirmation.length == 0) {
+    document.querySelector('.octicon-verified').classList.add('hide')
+    document.querySelector('.octicon-unverified').classList.add('hide')
+
+  }
+
+  if (password == password_confirmation) {
+      document.querySelector('.octicon-verified').classList.remove('hide')
+      document.querySelector('.octicon-unverified').classList.add('hide')
+    } else {
+      document.querySelector('.octicon-verified').classList.add('hide')
+      document.querySelector('.octicon-unverified').classList.remove('hide')
+    }
+}
