0a011e5da0b3060e3a1988c7e375a42525b79fc5
Rufanko <rufanko@mail.ru>
2020-01-02 08:18:48 +0300

    Add action and callback

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,5 +1,6 @@
 class Admin::TestsController < Admin::BaseController
-  before_action :find_test, only: %i[show update edit destroy start]
+  before_action :find_tests, only: %i[index update_inline]
+  before_action :find_test, only: %i[show update edit destroy start update_inline]
   def show; end
 
   def index
@@ -25,6 +26,14 @@ class Admin::TestsController < Admin::BaseController
     end
   end
 
+  def update_inline
+    if @test.update(test_params)
+      redirect_to admin_tests_path
+    else
+      render :index
+    end
+  end
+
   def create
     @test = current_user.authors_tests.new(test_params)
 
@@ -43,6 +52,10 @@ class Admin::TestsController < Admin::BaseController
 
   def find_test
     @test = Test.find(params[:id])
+  end
+
+  def find_tests
+    @tests = Test.all
   end
 
 end
