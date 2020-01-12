cc4d75842cfd54568a3cca2de37ea6758f69d9d1
Rufanko <rufanko@mail.ru>
2020-01-12 05:17:12 +0300

    Add badge service

---
+++ app/services/badge_service.rb
@@ -0,0 +1,35 @@
+class BadgeService
+
+  def initialize(test_passage)
+    @user = test_passage.user
+    @test = test_passage.test
+    @test_passage = test_passage
+  end
+
+  def call
+    Badge.all.each  do |badge|
+      reward!(badge) if send("#{badge.title}", rule)
+    end
+  end
+
+  def reward!(badge)
+    @user.badges.push(badge)
+  end
+
+  private
+
+  def first_try_badge(blank)
+    @user.tests.where(id: test.id).count == 1 && @test_passage.success?
+  end
+
+  def certain_level_badge(level)
+    @test_passage.success? && Test.all.where(level: level).count == @user.tests.where(level: level).uniq.count
+  end
+
+  def all_in_category_badge(category)
+    @test_passage.success? && Test.all_tests_by_category.count = @user.tests.where(category: category).uniq.count
+  end
+
+
+
+end
