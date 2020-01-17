54c70a49247564ba7a07dadf2ea9034469b13937
Rufanko <rufanko@mail.ru>
2020-01-17 08:38:07 +0300

    Upgrade badge service

--- app/services/badge_service.rb
+++ app/services/badge_service.rb
@@ -9,7 +9,7 @@ class BadgeService
 
   def call
     Badge.all.select do |badge|
-      send("#{badge.title}", badge.rule)
+      send("#{badge.title}", badge)
     end
   end
 
@@ -19,20 +19,34 @@ class BadgeService
     @user.tests.where(id: @test.id).count == 1 && @test_passage.success?
   end
 
-  def certain_level_badge(level)
-    return false if @user.rewarded?(@badge)
+  def certain_level_badge(badge)
+    level = badge.rule
+    return false unless @test_passage.success?
+    return false unless @test.level = level.to_i
+    if @user.rewarded?(badge)
+      last_badge_date = UserBadge.all.where(user: @user, badge: badge).order("created_at").last.created_at
 
-    if @test_passage.success? && @test.level = level.to_i
-      Test.all.where(level: level).count == @user.tests.where(level: level).uniq.count
+      all_tests_by_lvl = Test.all.where(level: level).count
+
+      all_tests_by_lvl == @user.test_passages.joins(:test)
+                               .where(passed: true)
+                               .where('created_at>?', last_badge_date)
+                               .where('tests.level=?', level).uniq.count
+    else
+      all_tests_by_lvl == @user.test_passages.joins(:test)
+                               .where(passed: true)
+                               .where('tests.level=?', level).uniq.count
     end
+
   end
 
-  def all_in_category_badge(category)
-    return false if @user.rewarded?(@badge)
+  def all_in_category_badge(badge)
+    category = badge.rule
+    category_id = Category.find_by(name: category).id
+    return false if @user.rewarded?(badge)
     return false unless @test_passage.success?
     return false unless Test.all_tests_by_category(category).count == @user.test_passages.joins(:test)
-                                                                                .where(passed: true)
-                                                                                .where('tests.category_id=?', category_id_by_name).count
-    category_id = Category.find_by(name: category).id
+                                                                           .where(passed: true)
+                                                                           .where('tests.category_id=?', category_id).count
   end
 end
