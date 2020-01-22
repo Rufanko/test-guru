4c03a65ff5a5f348cd91f2f23d7cb7faed7dbf85
Rufanko <rufanko@mail.ru>
2020-01-22 06:00:41 +0300

    Permit timer

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -14,7 +14,7 @@ class TestsController < ApplicationController
   private
 
   def test_params
-    params.require(:test).permit(:title, :level, :category_id)
+    params.require(:test).permit(:title, :level, :category_id, :timer)
   end
 
   def find_test
