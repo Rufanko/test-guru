42efe74258bde85606b491a037c6e9f365c74e52
Rufanko <rufanko@mail.ru>
2020-01-23 18:41:07 +0300

    Modify js code to show hours and minutes

--- app/javascript/utilities/timer.js
+++ app/javascript/utilities/timer.js
@@ -8,15 +8,24 @@ document.addEventListener('turbolinks:load', function() {
 function changeTimer(timer) {
     let testCreated = timer.dataset.testCreated,
         timeTest = timer.dataset.timeTest,
-        timeNow = (Date.now() / 1000).toFixed(),
+        timeNow = Math.trunc(Date.now() / 1000).toFixed(),
         passedTime = timeNow - testCreated
 
     if (passedTime > timeTest) {
         document.querySelector('.passage-form').submit()
         return
     }
+    var hours   = Math.trunc(passedTime / 3600)
+    var minutes = Math.trunc((passedTime - (hours * 3600)) / 60)
+    var seconds = passedTime - (hours * 3600) - (minutes * 60)
 
-    timer.textContent = timeTest - passedTime
+    if (seconds < 10) { seconds = '0' + seconds }
+
+    if (minutes < 10) { minutes = '0' + minutes }
+
+    if (hours < 10) { hours = '0' + hours }
+
+    timer.textContent = hours + ':' + minutes + ':' + seconds
 
     setTimeout(changeTimer, 1000, timer);
 }
